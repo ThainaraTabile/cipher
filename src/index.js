c10eaa0c4479683a01ffcf92051ae866b09173dd
Thainara Tabile <thainara.tabilebenevides@gmail.com>
2023-01-28 15:10:42 -0300

    implementa cipher

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+
+function cifrar() {
+  const textoCifrar = document.querySelector("#textoParaCriptografar").value.trim();
+  const offset = document.querySelector("#deslocaCifra").value;
+  const resultado = cipher.encode(textoCifrar, parseInt(offset));
+  document.querySelector("#textoCriptografado").innerText = resultado;
+}
+const inputCifrar = document.querySelector("#cifrar");
+inputCifrar.addEventListener("click", cifrar);
+
+function textoMaiusculo(event) {
+  const texto = event.target.value
+  event.target.value = texto.toUpperCase();
+}
+document.querySelector("#textoParaCriptografar").addEventListener("input", textoMaiusculo);
+
+const botaoCopiarC = document.querySelector("#copiarC");
+botaoCopiarC.addEventListener('click', function () {
+  const textAreaC = document.querySelector("#textoCriptografado");
+  textAreaC.select();
+  navigator.clipboard.writeText(textAreaC.value);
+});
+
+function decifrar() {
+  const textoCifrado = document.querySelector("#textoParaDescriptografar").value.trim();
+  const offset = document.querySelector("#deslocaDecifra").value;
+  const traduz = cipher.decode(textoCifrado, parseInt(offset));
+  document.querySelector("#textoDescriptografado").innerText = traduz;
+}
+const inputDecifrar = document.querySelector("#decifrar");
+inputDecifrar.addEventListener("click", decifrar);
+
+document.querySelector("#textoParaDescriptografar").addEventListener("input", textoMaiusculo)
+
+const botaoCopiarD = document.querySelector("#copiarD");
+botaoCopiarD.addEventListener('click', function () {
+  const textAreaD = document.querySelector("#textoDescriptografado");
+  textAreaD.select();
+  navigator.clipboard.writeText(textAreaD.value);
+});
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -25,8 +25,8 @@ describe('cipher', () => {
       expect(() => cipher.encode(0, 0)).toThrow(TypeError);
     });
 
-    it('should return "HIJKLMNOPQRSTUVWXYZABCDEFG" for "ABCDEFGHIJKLMNOPQRSTUVWXYZ" with offset 33', () => {
-      expect(cipher.encode(33, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')).toBe('HIJKLMNOPQRSTUVWXYZABCDEFG');
+    it('should return "DEFGHIJKLMNOPQRSTUVWXYZABC" for "ABCDEFGHIJKLMNOPQRSTUVWXYZ" with offset 3', () => {
+      expect(cipher.encode('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 3)).toBe('DEFGHIJKLMNOPQRSTUVWXYZABC');
     });
 
     // Hacker edition
@@ -39,24 +39,24 @@ describe('cipher', () => {
     // Se quiser adicionar testes para letras minúsculas, descomente o teste
     // abaixo.
     //
-    // it('should return "hijklmnopqrstuvwxyzabcdefg" for "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
-    //   expect(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz')).toBe('hijklmnopqrstuvwxyzabcdefg');
-    // });
-
-    // Hacker edition
-    //
-    // [Español]
-    // Si decides implementar soporte para caracteres no alfabéticos descomenta
-    // el test a continuación.
-    //
-    // [Português]
-    // Se quiser adicionar testes para caracteres não alfabéticos, descomente o
-    // teste abaixo.
-    //
-    // it('should return " !@" for " !@"', () => {
-    //   expect(cipher.encode(33, ' !@')).toBe(' !@');
-    // });
+    //it('should return "hijklmnopqrstuvwxyzabcdefg" for "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
+    //expect(cipher.encode('abcdefghijklmnopqrstuvwxyz', 33)).toBe('hijklmnopqrstuvwxyzabcdefg');
   });
+
+  // Hacker edition
+  //
+  // [Español]
+  // Si decides implementar soporte para caracteres no alfabéticos descomenta
+  // el test a continuación.
+  //
+  // [Português]
+  // Se quiser adicionar testes para caracteres não alfabéticos, descomente o
+  // teste abaixo.
+  //
+  //it('should return " !@" for " !@"', () => {
+  // expect(cipher.encode(' !@', 3)).toBe(' !@');
+  // });
+  //});
 
   describe('cipher.decode', () => {
 
@@ -71,8 +71,8 @@ describe('cipher', () => {
       expect(() => cipher.decode(0, 0)).toThrow(TypeError);
     });
 
-    it('should return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for "HIJKLMNOPQRSTUVWXYZABCDEFG" with offset 33', () => {
-      expect(cipher.decode(33, 'HIJKLMNOPQRSTUVWXYZABCDEFG')).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    it('should return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for "DEFGHIJKLMNOPQRSTUVWXYZABC" with offset 3', () => {
+      expect(cipher.decode('DEFGHIJKLMNOPQRSTUVWXYZABC', 3)).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
     });
 
     //
@@ -86,23 +86,21 @@ describe('cipher', () => {
     // Se quiser adicionar testes para letras minúsculas, descomente o teste
     // abaixo.
     //
-    // it('should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
-    //   expect(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz');
-    // });
-
-    // Hacker edition
-    //
-    // [Español]
-    // Si decides implementar soporte para caracteres no alfabéticos descomenta
-    // el test a continuación.
-    //
-    // [Português]
-    // Se quiser adicionar testes para caracteres não alfabéticos, descomente o
-    // teste abaixo.
-    //
-    // it('should return " !@" para " !@"', () => {
-    //   expect(cipher.decode(33, ' !@')).toBe(' !@');
-    // });
+    ///it('should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
+    // expect(cipher.decode('hijklmnopqrstuvwxyzabcdefg', 33)).toBe('abcdefghijklmnopqrstuvwxyz');
   });
 
+  // Hacker edition
+  //
+  // [Español]
+  // Si decides implementar soporte para caracteres no alfabéticos descomenta
+  // el test a continuación.
+  //
+  // [Português]
+  // Se quiser adicionar testes para caracteres não alfabéticos, descomente o
+  // teste abaixo.
+  //
+  // it('should return " !@" para " !@"', () => {
+  // expect(cipher.decode(' !@', 33)).toBe(' !@');
+  // });
 });
